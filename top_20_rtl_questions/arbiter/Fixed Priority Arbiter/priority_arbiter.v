4f1d33e09ed45d2e83ee593e1cda58a1c8d6c19d
harsha-xilinx <103888958+harsha-xilinx@users.noreply.github.com>
2025-10-31 08:19:07 +0530

    Update priority_arbiter.v

--- top_20_rtl_questions/arbiter/Fixed Priority Arbiter/priority_arbiter.v
+++ top_20_rtl_questions/arbiter/Fixed Priority Arbiter/priority_arbiter.v
@@ -1,32 +1,59 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 31.10.2025 07:52:40
+// Design Name: 
+// Module Name: priority_arbiter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
 module priority_arbiter #(
     parameter N = 4   // number of request lines
 )(
     input  wire [N-1:0] req,    // request vector
     output reg  [N-1:0] grant   // one-hot grant vector
 );
+integer i;
+reg found;
   always@ *
   begin
-    grant = {N(1'b0)};
+    found=1'b0;
+    grant = {N{1'b0}};
     for (i=0; i<N; i=i+1)
+     begin
+     if(!found)
      begin
       if(req[i])
        begin  
         grant[i] = 1'b1;
-        disable for;
+        found=1'b1;
        end
+      end
      end
   end 
 endmodule
 
 module tb_priority_arbiter;
     parameter N = 4;  // can change to any number of requesters
-
     reg  [N-1:0] req;
     wire [N-1:0] grant;
   priority_arbiter pa1 (.req(req), .grant(grant));
 
   reg [N-1:0] expected_value; 
-  
+integer i,j;  
   initial
     begin
       $display("Time\tReq\tGrant\tExpected\tResult");
@@ -34,19 +61,19 @@ module tb_priority_arbiter;
       for(i=0; i<30; i=i+1)
       begin
           #10; req= $random;
-        
+ 
     //computing expected values
       for(j=0; j<N; j=j+1)
         begin
           if (req[j])
             begin
-              expected[j] = 1'b1;
+              expected_value[j] = 1'b1;
             end
         end
-    if (grant === expected)
-                $display("%0t\t%b\t%b\t%b\tPASS", $time, req, grant, expected);
+    if (grant === expected_value)
+                $display("%0t\t%b\t%b\t%b\tPASS", $time, req, grant, expected_value);
     else
-                $display("%0t\t%b\t%b\t%b\tFAIL", $time, req, grant, expected);
+                $display("%0t\t%b\t%b\t%b\tFAIL", $time, req, grant, expected_value);
     end
   #200; $finish;
   end   
